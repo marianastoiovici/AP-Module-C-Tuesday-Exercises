43e8d60311a0a50ab3799b4a5bbab37ddb3218bc
marianastoiovici <56549060+marianastoiovici@users.noreply.github.com>
2021-01-13 19:12:36 +0000

    Revert to previous solution to follow constraints of exercise.

--- exercise3.cpp
+++ exercise3.cpp
@@ -1,28 +1,43 @@
-// C++ program that checks whether a word from the input is a palindrome and disolays a message acordingly.
+// C++ program that checks whether a word is a palindrome.
 // https://repl.it/@mstoiovici/PalindromeChecker1#main.cpp
 #include <iostream>
-#include <string>
 using namespace std;
 
 
 int main() {
-  string input;
-  int start = 0;
-  
+  string userInput;
+  string userInputCopy;
+  string userInputLower = "";
+  string reversedUserInput = "";
+
   cout << "Please enter a word: ";
-  getline(cin, input);
-  int end = input.size()-1;
-  
-  while(start<end) {
-    if(tolower(input[start]) != tolower(input[end])){
-      cout << "Sorry, "<< "\'" << input << "\' is not a palindrome." << endl;
-      return 0;
-    } else {
-      start++;
-      end--;
-    }
+  getline(cin, userInput);
+  //make a copy of the userInput
+  userInputCopy = userInput;
+
+  //loop through the userInputCopy, converts its characters to lower case and concatenate them to userInputLower
+  for(int i=0; i<userInputCopy.length(); i++){
+    userInputCopy[i] = tolower(userInputCopy[i]);
+    userInputLower = userInputLower + userInputCopy[i];
   }
-  cout << "Yes, "<< "\'" << input << "\' is a palindrome \n" << endl;
- 
+
+  // loop backwords through the userInputLower, from last character and concatenate it to reversedUserInput
+  for(int i=userInputLower.length()-1; i>=0; i--){
+    reversedUserInput = reversedUserInput + userInputLower[i];
+  }
+  
+  // check if userInput is a palindrome by comparing all characters of its lowered version with the one of its reversed version
+  int i=0;
+  while (i<userInputLower.length()){
+    if(userInputLower[i] != reversedUserInput[i]){
+      cout << "Sorry, "<< "\'" << userInput << "\' is not a palindrome." << endl;;
+      i=userInputLower.length();
+      return 0;
+    }
+    i++;
+  }
+
+  cout << "Yes, "<< "\'" << userInput << "\' is a palindrome \n" << endl;
+
   return 0;
 }
